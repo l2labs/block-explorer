1e48f6244a20d0eca2c60ffc51282da51363f723
wuww <wuweiwei@web3.com>
2024-05-06 13:51:56 +0800

    feat: build env

--- packages/app/src/composables/useRuntimeConfig.ts
+++ packages/app/src/composables/useRuntimeConfig.ts
@@ -2,6 +2,6 @@ import type { RuntimeConfig } from "@/configs";
 
 export default (): RuntimeConfig => {
   return {
-    appEnvironment: import.meta.env?.APP_MODE || "local",
+    appEnvironment: APP_MODE || "local",
   };
 };
--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -64,5 +64,6 @@ export default defineConfig({
     __VUE_I18N_FULL_INSTALL__: true,
     __VUE_I18N_LEGACY_API__: false,
     __INTLIFY_PROD_DEVTOOLS__: false,
+    APP_MODE: JSON.stringify(process.env.APP_MODE || "local"),
   },
 });
